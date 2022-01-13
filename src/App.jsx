542b026e74570ebb84b6d5328ed1257ab3b82e47
Dominic Petriella <50121548+dompetriella@users.noreply.github.com>
2022-01-13 21:51:32 +0000

    added borders to svg and rough drag drop functionality

--- src/App.jsx
+++ src/App.jsx
@@ -29,12 +29,29 @@ const iconColors = [
     blueFrog, goldFrog, greenFrog, orangeFrog, pinkFrog
 ]
 
+//gives the svg a border
+const getFilterCSS = color => {
+    return `drop-shadow(2px 0 0 ${color}) drop-shadow(-2px 0 0 ${color}) drop-shadow(0 2px 0 ${color}) drop-shadow(0 -2px 0 ${color})`
+}
+
+// currently all black until I want to change it
+const getFrogOutline = iconColor => {
+    if (iconColor === blueFrog) return getFilterCSS('black')
+    if (iconColor === orangeFrog)  return getFilterCSS('black')
+    if (iconColor === greenFrog) return getFilterCSS('black')
+    if (iconColor === goldFrog)  return getFilterCSS('black')
+    if (iconColor === pinkFrog)  return getFilterCSS('black')
+
+}
+
 let threeRowExclude = []
 let fourRowExclude = []
 
 function App() {
 
     const [iconColorsGrid, setIconColorsGrid] = useState([])
+    const [draggedIcon, setDraggedIcon] = useState(null)
+    const [replacedIcon, setReplacedIcon] = useState(null)
 
     function createGrid() {
         const randomIconColorsList = []
@@ -44,7 +61,6 @@ function App() {
         }
 
         setIconColorsGrid(randomIconColorsList)
-        console.log(iconColorsGrid)
     }
 
     function setRowExclusions() {
@@ -65,11 +81,9 @@ function App() {
 
     const threeColumnMatch = () => {
 
-        for (let i = 0; i < (grid-(width * 2)-1); i++) {
+        for (let i = 0; i <= (grid-(width * 2)-1); i++) {
             const column = [i, i + width, i + (width * 2)]
             const columnColor = iconColorsGrid[i]
-            console.log(columnColor)
-            console.log(column)
 
             if (column.every(icon => iconColorsGrid[icon] === columnColor)) {
                 column.forEach(icon => iconColorsGrid[icon] = noFrog)
@@ -79,7 +93,7 @@ function App() {
 
     const fourColumnMatch = () => {
 
-        for (let i = 0; i < (grid-(width * 3)-1); i++) {
+        for (let i = 0; i <= (grid-(width * 3)-1); i++) {
             const column = [i, i + width, i + (width * 2), i + (width * 3),]
             const columnColor = iconColorsGrid[i]
 
@@ -118,7 +132,7 @@ function App() {
         }
     }
 
-    const dropIcon = () => {
+    const moveIconDown = () => {
         for (let i = 0; i < (grid - width); i++) {
 
             if (i < width && iconColorsGrid[i] === noFrog) {
@@ -139,23 +153,52 @@ function App() {
             fourRowMatch()
             threeColumnMatch()
             threeRowMatch()
-            dropIcon()
+            moveIconDown()
             setIconColorsGrid([...iconColorsGrid])
             }, 300)
             
         return () => clearInterval(activeTimer)
-    }, [fourColumnMatch, fourRowMatch, threeColumnMatch, threeRowMatch, dropIcon, iconColorsGrid])
+    }, [fourColumnMatch, fourRowMatch, threeColumnMatch, threeRowMatch, moveIconDown, iconColorsGrid])
 
+
+    const dragStart = (e) => {
+        setDraggedIcon(e.target)
+    }
+
+    const dragDrop = (e) => {
+        setReplacedIcon(e.target)
+    }
+
+    const dragEnd = () => {
+
+        const iconBeingDraggedIndex = parseInt(draggedIcon.getAttribute('data-id'))
+        const replacedIconIndex = parseInt(replacedIcon.getAttribute('data-id'))
+
+        iconColorsGrid[replacedIconIndex] = draggedIcon.getAttribute('src')
+        iconColorsGrid[iconBeingDraggedIndex] = replacedIcon.getAttribute('src')
+
+        const validMoves = [
+            
+        ]
+    }
 
     return (
         <main className="app">
             <div className="game">
                 {iconColorsGrid.map((iconColors, index) => (
                     <img
-                        src={iconColors}
+                        src = {iconColors}
+                        style = {{filter: getFrogOutline(iconColors)}}
                         key = {index}
-                        id = {index}
-                        style = {{backgroundColor: iconColors}}
+                        data-id = {index}
+                        data-color = {iconColors}
+                        draggable = {true}
+                        onDragOver = {(e) => e.preventDefault()}
+                        onDragEnter = {(e) => e.preventDefault()}
+                        onDragLeave = {(e) => e.preventDefault()}
+                        onDragStart = {dragStart}
+                        onDrop = {dragDrop}
+                        onDragEnd = {dragEnd}
                     />
                 ))}
             </div>
